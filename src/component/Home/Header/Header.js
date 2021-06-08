406e70bf352eee3d75e8b0a08c41842dd228cc22
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-06-08 21:26:31 +0600

    edit header

--- src/component/Home/Header/Header.js
+++ src/component/Home/Header/Header.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import logo from "../../../images/logo.png"
 import './Header.css'
 
 const Header = () => {
     return (
-        <Navbar bg="transparent" expand="lg">
+        <Navbar style={{position: 'absolute', left: 0, top: 0, right: 0, zIndex: 3 }} bg="transparent" expand="lg">
             <Container>
-                <Navbar.Brand as={Link} className="web-name header-logo" to="/"><img src={logo} alt=""/></Navbar.Brand>
+                <Navbar.Brand as={Link} style={{fontSize: '40px'}} className="web-name text-white header-logo" to="/">Go Wash</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
-                        <Nav.Link as={Link} to="/home">Home</Nav.Link>
-                        <Nav.Link as={Link} to="#">About us</Nav.Link>
-                        <Nav.Link as={Link} to="#">Projects</Nav.Link>
-                        <Nav.Link as={Link} to="#">Contact</Nav.Link>
-                        <Nav.Link as={Link} to="/sidebar">Admin</Nav.Link>
-                        <Nav.Link as={Link} to="/login" className="active btn-brand p-1 px-4">Login</Nav.Link>
+                        <Nav.Link className=" text-white" as={Link} to="/home">Home</Nav.Link>
+                        <Nav.Link className=" text-white" as={Link} to="#">About us</Nav.Link>
+                        <Nav.Link className=" text-white" as={Link} to="#">Projects</Nav.Link>
+                        <Nav.Link className=" text-white" as={Link} to="#">Contact</Nav.Link>
+                        <Nav.Link className=" text-white" as={Link} to="/sidebar">Admin</Nav.Link>
+                        <Nav.Link className=" text-white" as={Link} to="/login" className="active btn-brand p-1 px-4">Login</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
